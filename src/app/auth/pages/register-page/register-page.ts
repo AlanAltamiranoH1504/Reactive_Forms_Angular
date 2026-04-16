d98732e3e35659aa9c6449a06c7b3ca9ebbbeecb
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2026-04-16 13:36:58 -0600

    feat: add validation handling using regular expressions

--- src/app/auth/pages/register-page/register-page.ts
+++ src/app/auth/pages/register-page/register-page.ts
@@ -1,12 +1,45 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormValidatedService } from '../../../services/forma-validated-service';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css',
 })
 export class RegisterPage {
+  protected formBuilder = inject(FormBuilder);
+  protected formValidatorService = inject(FormValidatedService);
+
+  protected registerForm: FormGroup = this.formBuilder.group({
+    name: ["", [Validators.required, Validators.minLength(3), Validators.pattern(this.formValidatorService.namePattern)]],
+    email: ["", [Validators.required, Validators.pattern(this.formValidatorService.emailPattern)]],
+    username: ["", [Validators.required, Validators.minLength(6), Validators.pattern(this.formValidatorService.notOnlySpacesPattern)]],
+    password: ["", [Validators.required, Validators.minLength(6)]],
+    confirmedPassword: [null, [Validators.required]]
+  }, {
+    Validators: {
+
+    }
+  });
+
+  protected onSubmit() {
+    if(this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
+    console.log("Extrauendo la data");
+    
+  }
+
+  protected isFieldOneEqualsFieldTwo(fieldOne: string, fieldTwo: string) {
+    () => {
+      
+    }
+    return null;
+  }
 
 }
--- src/app/services/forma-validated-service.ts
+++ src/app/services/forma-validated-service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
-import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+import { AbstractControl, FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FormValidatedService {
+  public namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  public notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
   public isValidField(fieldName: string, myForm: FormGroup) {
     return myForm.controls[fieldName].errors && myForm.controls[fieldName].touched;
   }
@@ -31,14 +35,29 @@ export class FormValidatedService {
       switch (key) {
         case 'required':
           return 'Este campo es requerido';
-
         case 'minlength':
           return `El campo debe tener minimo ${errors['minlength'].requiredLength} caracteres`;
-
         case 'min':
           return `El valor minimo del campo debe ser ${errors['min'].min}`;
+        case 'email':
+          return 'El campo de email no es valido';
+        case 'pattern':
+          if (errors['pattern'].requiredPattern === this.namePattern) {
+            return 'El nombre debe tener formato de nombre y apellidos';
+          } else if (errors['pattern'].requiredPattern === this.emailPattern) {
+            return 'El email no tiene un formato valido';
+          } else if (errors['pattern'].requiredPattern === this.notOnlySpacesPattern) {
+            return 'El username no puede tener espacios en blanco';
+          }
+          return "Error de patron contra expresion regular"
+        default:
+          return 'Error no controlado';
       }
     }
     return null;
+  }
+
+  public async checkynServerResponse(control: AbstractControl) {
+
   }
 }
