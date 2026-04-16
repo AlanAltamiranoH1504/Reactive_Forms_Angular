97e5afc2f619b54e46fc78ada46d6c7dafbf9dc6
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2026-04-16 12:26:57 -0600

    feat: add support for React forms with radio buttons and checkboxes

--- src/app/reactive/pages/switches-page/switches-page.ts
+++ src/app/reactive/pages/switches-page/switches-page.ts
@@ -1,12 +1,26 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormValidatedService } from '../../../services/forma-validated-service';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './switches-page.html',
   styleUrl: './switches-page.css',
 })
 export class SwitchesPage {
+  protected formValidatorService = inject(FormValidatedService);
 
+  protected formBuilder = inject(FormBuilder);
+  protected formSwitches: FormGroup = this.formBuilder.group({
+    gender: ["M", [Validators.required]], // Radio button default de genero 
+    wantNotifications: [true], // Radio button de notificaciones por default a true
+    termsAndConditions: [false, Validators.requiredTrue]
+  });
+
+  onSubmit() {
+    this.formSwitches.markAllAsTouched()
+    
+  }
 }
